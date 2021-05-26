d71e44bcf3414357860833d3df9357182336ff44
steftes99 <stefanotescione83@gmail.com>
2021-05-26 15:11:38 +0200

    esercizio finito

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Qual è il tuo colore preferito?');
+
+
+
+document.getElementById('password').innerHTML = nome + cognome + colore + 21;
+
